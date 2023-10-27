4a4bb4b95546518e5be41b5ce8baf1a3d161c4ec
Steve <goldenskgaming@gmail.com>
2023-10-26 20:36:14 -0700

    Finished Game
    * Added more Traps
    * Balanced Projectile Tiles
    * Made level 3 harder
    * Made acid box collider more accurate

--- Haunted Candy Escape/Assets/Source/Scripts/Traps.cs
+++ Haunted Candy Escape/Assets/Source/Scripts/Traps.cs
@@ -6,11 +6,15 @@ using UnityEngine.Tilemaps;
 public class Traps : MonoBehaviour
 {
     public Transform Spike;
+    public float InitialFireMin = 1f;
+    public float InitialFireMax = 10f;
+    public float IntervalFireMin = 2.5f;
+    public float IntervalFireMax = 6.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Fire", Random.Range(1f, 10f), Random.Range(2.5f, 6f));
+        InvokeRepeating("Fire", Random.Range(InitialFireMin, InitialFireMax), Random.Range(IntervalFireMin, IntervalFireMax));
     }
 
     private void Fire() {
